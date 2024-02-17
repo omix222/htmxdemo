5d86876ea5c9bffc7df8bf0887b79ed1990a85e8
Hiromi Takahashi <hiromix222@gmail.com>
2024-02-17 15:03:59 +0900

    first commit

---
+++ src/main/java/com/example/htmxdemo/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.htmxdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+}
---
+++ src/main/java/com/example/htmxdemo/HtmxdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmxdemoApplication.class, args);
+	}
+
+}
